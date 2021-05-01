b563118b7b4fdb58b807966f77ece3108532521b
Prathap Beschi A A <pbeski@gmail.com>
2021-05-01 20:59:50 +0530

    Header and Footer added with basic layout test

---
+++ components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div>
+      <p>Based on nuxt/bootstrap-vue docs.</p>
+    </div>
+  </footer>
+</template>
+
+<style>
+footer {
+  border-top: 3px solid #dee2e6;
+  text-align: center;
+  background-color: #eee;
+  padding: 1rem 0;
+}
+footer p {
+
+  margin: 0
+}
+</style>
